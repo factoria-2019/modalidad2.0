6811df9c1276b1e6389f041653d866419bf26b08
AndresPerez <edgar4698@hotmail.com>
2019-12-11 05:11:37 -0500

    validaciones Registro 2-3

--- Desarrollo/BPUS/src/app/pages/semillero/form2/form2.component.ts
+++ Desarrollo/BPUS/src/app/pages/semillero/form2/form2.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators,ReactiveFormsModule,FormArray } from '@angular/forms';
+import { Component, OnInit, NgModule } from '@angular/core';
+import {Router} from '@angular/router';
 
 
 @Component({
@@ -6,11 +8,45 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './form2.component.html',
   styles: []
 })
+@NgModule({
+  imports:[
+    ReactiveFormsModule
+  ],
+})
 export class Form2Component implements OnInit {
+  isSubmitted = false;
 
-  constructor() { }
+constructor(public fb:FormBuilder, private router: Router) { }
+registrationForm2 = this.fb.group({
+ NombreTutor:['',[Validators.required]],
+ Actividades:['',[Validators.required]]
 
+})
+
+onSubmit() {
+  this.isSubmitted = true;
+  if (!this.registrationForm2.valid) {
+      console.log(this.registrationForm2.value);
+    return false;
+
+  } else {
+
+    this.router.navigate(['/semillero/form3']);
+
+  }
+
+
+
+}
   ngOnInit() {
   }
+  tutores = [
+    { id: 0, name: 'Juan Castro' },
+    { id: 1, name: 'Ferley Medina' },
+    { id: 2, name: 'Jhon Jerson Robles' },
+    { id: 4, name: 'Andres Anaya' },
+    { id: 5, name: 'Fernando Rojas' },
+    { id: 6, name: 'Eliecer Martinez' },
+  ]
 }
 
--- Desarrollo/BPUS/src/app/pages/semillero/form3/form3.component.ts
+++ Desarrollo/BPUS/src/app/pages/semillero/form3/form3.component.ts
@@ -1,16 +1,40 @@
-import { Component, OnInit } from '@angular/core';
-
+import { FormBuilder, Validators,ReactiveFormsModule,FormArray } from '@angular/forms';
+import { Component, OnInit, NgModule } from '@angular/core';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'form3',
   templateUrl: './form3.component.html',
   styles: []
 })
+@NgModule({
+  imports:[
+    ReactiveFormsModule
+  ],
+})
 export class Form3Component implements OnInit {
+  isSubmitted = false;
 
-  constructor() { }
+  constructor(public fb:FormBuilder, private router: Router) { }
+  registrationForm3 = this.fb.group({
+  NombreProyecto:['',[Validators.required]],
+  ObjetivoGp:['',[Validators.required]]
 
+  })
+  onSubmit() {
+    this.isSubmitted = true;
+    if (!this.registrationForm3.valid) {
+        console.log(this.registrationForm3.value);
+      return false;
+
+    } else {
+
+      this.router.navigate(['/semillero/form4']);
+
+    }
+  }
   ngOnInit() {
   }
-}
 
+
+}
